89d2a5bed25e0f4747f0d3ddd8701095f5247156
Lovatiana15 <hei.lovatiana@gmail.com>
2023-12-12 23:29:00 +0300

    project initialize by poja

---
+++ src/main/java/com/lva/base/repository/DummyRepository.java
@@ -0,0 +1,15 @@
+package com.lva.base.repository;
+
+import com.lva.base.PojaGenerated;
+import com.lva.base.repository.model.Dummy;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@PojaGenerated
+@Repository
+public interface DummyRepository extends JpaRepository<Dummy, String> {
+
+  @Override
+  List<Dummy> findAll();
+}
